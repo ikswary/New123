d44c0d46e45890a6dc2a2a851c76385ec80181d5
SJH <jinhyeon.son@voithru.com>
2022-11-22 15:37:13 +0900

    feat : add base project files

---
+++ src/main/kotlin/com/liner/highlight/HighlightApiApplication.kt
@@ -0,0 +1,11 @@
+package com.liner.highlight
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighlightApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HighlightApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/liner/highlight/HighlightApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.liner.highlight
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighlightApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
